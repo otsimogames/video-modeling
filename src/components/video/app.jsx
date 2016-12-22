130f4473fc17a99d22e33c879c4439365e7af775
btk <iletisim@buraktokak.com>
2016-12-22 14:34:12 +0300

    Style components

--- src/components/video/app.jsx
+++ src/components/video/app.jsx
@@ -2,9 +2,19 @@ import styles from './index.scss';
 import React from 'react';
 
 export default class Video extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  videoSlug = () => {
+    return "data/videos/"+this.props.id +"-1-1.mp4";
+  }
   render() {
     return (
-      <div>Video Here</div>
+      <div className={styles.video}>
+        <video src={this.videoSlug()} type="video/mp4" controls>
+          Your browser does not support the video tag.
+        </video>
+      </div>
     )
   }
 }
