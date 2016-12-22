79eca494b0615a281b81df8f153e82e373da9543
btk <iletisim@buraktokak.com>
2016-12-22 11:34:32 +0300

    First commit.

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,11 @@
 import styles from './index.scss';
 import React from 'react';
+import VideoHolder from './components/video-holder/app.jsx';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>It Works!</h1>
-        <p>This React project just works including <span className={styles.redBg}>module</span> local styles.</p>
-        <p>Enjoy!</p>
-      </div>
+      <VideoHolder />
     )
   }
 }
---
+++ src/components/video/app.jsx
@@ -0,0 +1,10 @@
+import styles from './index.scss';
+import React from 'react';
+
+export default class Video extends React.Component {
+  render() {
+    return (
+      <div>Video Here</div>
+    )
+  }
+}
