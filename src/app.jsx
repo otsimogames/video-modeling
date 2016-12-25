cca65a71ca83e3d7627b95ddecb44bb070487043
btk <iletisim@buraktokak.com>
2016-12-25 17:08:59 +0300

    Add videoQuantity (difficulty) logic

--- src/app.jsx
+++ src/app.jsx
@@ -2,11 +2,14 @@ import styles from './index.scss';
 import React from 'react';
 import VideoHolder from './components/video-holder/app.jsx';
 
-
 export default class App extends React.Component {
-  render() {
-    return (
-      <VideoHolder />
-    )
-  }
+	constructor() {
+		super();
+		this.videoQuantity = 3;
+	}
+	// NOTE: Shift this to difficulty later on.
+
+	render() {
+		return (<VideoHolder videoQuantity={this.videoQuantity}/>)
+	}
 }
--- src/components/video-holder/app.jsx
+++ src/components/video-holder/app.jsx
@@ -3,32 +3,53 @@ import React from 'react';
 import Video from '../video/app.jsx';
 
 export default class VideoHolder extends React.Component {
-  animatePlayVideos(){
-    var video1 = document.getElementById("video1");
-    var video2 = document.getElementById("video2");
-    var video3 = document.getElementById("video3");
+	constructor(props) {
+		super(props);
+		this.videos = [];
+	}
 
-    
-    video1.play();
-    video1.onended = () => {
-      video2.play();
-      video2.onended = () => {
-        video3.play();
-        video3.onended = () => {
-          alert("all ended");
-        }
-      }
-    }
-  }
+	componentDidMount() {
+		for (var i = 0; i < this.props.videoQuantity; i++) {
+			this.videos.push(document.getElementById('video' + parseInt(i + 1)));
+		}
+	}
 
-  render() {
-    return (
-      <div className={styles.videoHolder}>
-        <Video id="1" slug="1-2-1"/>
-        <Video id="2" slug="1-2-2"/>
-        <Video id="3" slug="1-2-3"/>
-        <button onClick={this.animatePlayVideos} >Animate</button>
-      </div>
-    )
-  }
+	animatePlayVideos() {
+		let i = 0;
+		this.videos.forEach((vid) => {
+			vid.onended = () => {
+				this.playNext(vid.id);
+			}
+			i++;
+		});
+
+		// Start Animation
+		this.videos[0].play();
+	}
+
+	playNext(vidId) {
+		let video2Play = this.videos[parseInt(vidId.replace('video', ''))];
+		if (video2Play) {
+			video2Play.play();
+		} else {
+			console.log("All Ended");
+		}
+	}
+
+	prepVideos() {
+		var videos = [];
+		for (var i = 0; i < this.props.videoQuantity; i++) {
+			videos.push(<Video id={i + 1} key={i + 1} slug="1-2-1"/>);
+		}
+		return videos;
+	}
+
+	render() {
+		return (
+			<div className={styles.videoHolder}>
+				{this.prepVideos()}
+				<button onClick={this.animatePlayVideos.bind(this)}>Animate</button>
+			</div>
+		)
+	}
 }
